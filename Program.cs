c6447c6e870dde706220468c99eb7951a669ee65
Maria Luiza <malualper@hotmail.com>
2020-05-24 09:59:32 -0300

    fgts

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fgts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Calcular a parcela do FGTS sobre o salário de um funcionário 8%.");
+            Console.WriteLine("Sálario: R$ 2000,00");
+            double resultado=2000 * 8;
+            double resultado2=resultado / 100;
+            
+            Console.WriteLine($"FGTS: R$ {resultado2},00");
+        }
+    }
+}
